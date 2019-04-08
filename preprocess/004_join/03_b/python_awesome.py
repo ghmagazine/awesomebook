9cf57474e6e3e70e1de1f9ed117131a0b9036eca
takuya <takuya.takaya@gmail.com>
2019-04-08 19:22:51 +0900

    P112 python_awesome.py

--- preprocess/004_join/03_b/python_awesome.py
+++ preprocess/004_join/03_b/python_awesome.py
@@ -3,21 +3,21 @@ import pandas as pd
 customer_tb, hotel_tb, reserve_tb = load_hotel_reserve()
 
 # 下の行から本書スタート
-# 予約テーブルにprice_avgを追加
-reserve_tb['price_sum'] = pd.Series(
-  reserve_tb
+# customer_idごとにreserve_datetimeでデータを並び替え
+result = reserve_tb.groupby('customer_id') \
+  .apply(lambda x: x.sort_values(by='reserve_datetime', ascending=True)) \
+  .reset_index(drop=True)
 
-    # データ行をcustomer_idごとにグループ化
+# 新たな列としてprice_sumを追加
+result['price_sum'] = pd.Series(
+    # 必要なデータ列のみに絞り込み
+    result.loc[:, ["customer_id", "total_price"]]
+
+    # customer_idごとにtotal_priceのwindow3件にまとめ、その合計値を計算
     .groupby('customer_id')
-
-    # customer_idごとにreserve_datetimeでデータを並び替え
-    .apply(lambda x: x.sort_values(by='reserve_datetime', ascending=True))
-
-    # total_priceのwindow3件にまとめ、その合計値を計算
-    .loc[:, 'total_price']
     .rolling(center=False, window=3, min_periods=3).sum()
 
-    # group化を解除すると同時に、customer_idの列を削除
+    # group化を解除すると同時に、total_priceの列を取り出し
     .reset_index(drop=True)
+    .loc[:, 'total_price']
 )
-
